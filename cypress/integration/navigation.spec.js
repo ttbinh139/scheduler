6da0525bc0c4a4a2c301c775f326d8f7194193cf
Binh Trinh <ttbinh139@gmail.com>
2022-06-22 18:23:57 -0700

    Applied Cypress to perform intergration testing

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,16 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    /* cy.contains("li", "Tuesday").click();
+    cy.contains("li", "Tuesday")
+      .should("have.css", "background-color", "rgb(242, 242, 242)"); */
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected")
+  });
+
+});
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -18,7 +18,7 @@ export default function DayListItem(props) {
     }
   }
   return (
-    <li className={dayClass} selected={props.selected} onClick={() => {props.setDay(props.name)}}>
+    <li data-testid="day" className={dayClass} selected={props.selected} onClick={() => {props.setDay(props.name)}}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
